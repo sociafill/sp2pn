c5d763b7362a8812284e73e87aed030a90629d80
Johnny Debt <38006769+johnny-debt@users.noreply.github.com>
2018-07-25 00:37:06 +0300

    Refactoring and cleanup

--- broker.go
+++ broker.go
@@ -1,22 +1,24 @@
 package sp2pt
 
 import (
-	"fmt"
 	"time"
 )
 
+type identifiersMap map[Identifier]int
+type identifiersControl map[Identifier]chan bool
+
 // Broker is main structure which allows to manage observables, subscriptions, consumer
 type Broker struct {
-	identifiers map[Identifier]int
-	observables map[Identifier]chan bool
+	identifiers identifiersMap
+	observables identifiersControl
 	consumer    Consumer
 }
 
 // NewBroker is constructor for Broker
 func NewBroker(consumer Consumer) Broker {
 	broker := Broker{}
-	broker.identifiers = map[Identifier]int{}
-	broker.observables = map[Identifier]chan bool{}
+	broker.identifiers = identifiersMap{}
+	broker.observables = identifiersControl{}
 	broker.consumer = consumer
 	return broker
 }
@@ -24,8 +26,7 @@ func NewBroker(consumer Consumer) Broker {
 // Watch function is used to initiate watching to some observable object
 func (broker *Broker) Watch(observable Observable) {
 	broker.identifiers[observable.Identifier()]++
-	_, isRunning := broker.observables[observable.Identifier()]
-	if !isRunning {
+	if !broker.isRunning(observable) {
 		broker.runPolling(observable)
 	}
 }
@@ -33,18 +34,20 @@ func (broker *Broker) Watch(observable Observable) {
 // Unwatch function is used to decrement watchers counter for some observable object.
 // If some observable object has no watchers - polling must be stopped
 func (broker *Broker) Unwatch(observable Observable) {
-	fmt.Printf("Stop watching %s\n", observable.Identifier())
 	broker.identifiers[observable.Identifier()]--
-	if broker.identifiers[observable.Identifier()] <= 0 {
-		_, isRunning := broker.observables[observable.Identifier()]
-		if isRunning {
+	if broker.identifiers[observable.Identifier()] < 1 {
+		if broker.isRunning(observable) {
 			broker.stopPolling(observable)
 		}
 	}
 }
 
+func (broker *Broker) isRunning(observable Observable) bool {
+	_, isRunning := broker.observables[observable.Identifier()]
+	return isRunning
+}
+
 func (broker *Broker) runPolling(observable Observable) {
-	fmt.Printf("Run watcher for %s\n", observable.Identifier())
 	channel := make(chan bool)
 	broker.observables[observable.Identifier()] = channel
 	go polling(channel, observable, broker.consumer)
@@ -59,14 +62,11 @@ func polling(stop chan bool, observable Observable, consumer Consumer) {
 	for {
 		select {
 		default:
-			// fmt.Printf("Poll of [%s] has new iteration\n", observable.Identifier())
 			items := observable.Poll()
-			// fmt.Printf("Items received\n")
 			for _, item := range items {
 				consumer.Consume(observable, item)
 			}
 		case <-stop:
-			fmt.Printf("Signal stop received for watcher of [%v]\n", observable.Identifier())
 			return
 		}
 		time.Sleep(observable.GetInterval())
